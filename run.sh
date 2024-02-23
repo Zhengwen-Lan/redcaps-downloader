538861db5da11f6712da18711962b7fcd71d5005
Zhengwen-Lan <55707415+Zhengwen-Lan@users.noreply.github.com>
2024-02-23 18:39:16 +0800

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-python runforGit.py --start 100 --end 200 --processNum 32 --output 'data/image_100'
-python runforGit.py --start 200 --end 300 --processNum 32 --output 'data/image_200'
-python runforGit.py --start 300 --end 400 --processNum 32 --output 'data/image_300'
-# python runforGit.py --start 100 --end 200 --processNum 64 --output 'data/image_100'
+python runforGit.py --start 150 --end 175 --processNum 12 --output 'data/150_175'
+python runforGit.py --start 175 --end 200 --processNum 12 --output 'data/175_200'
+python runforGit.py --start 200 --end 250 --processNum 12 --output 'data/200_250'
